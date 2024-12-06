6996bd002c36f87e67c142e07c5ee507742c2313
kim-taewoo <acoustic0419@gmail.com>
2024-12-06 12:15:25 +0900

    chore: tailwind config 파일 import 에러 해결

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
+import animate from "tailwindcss-animate";
 
 export default {
   darkMode: ["class"],
@@ -63,5 +64,5 @@ export default {
       }
     }
   },
-  plugins: [require("tailwindcss-animate")]
+  plugins: [animate]
 } satisfies Config;
