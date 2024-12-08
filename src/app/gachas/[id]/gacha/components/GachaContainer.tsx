4ddb0db183bed10a2cab31445341674030e826aa
jes <suni980215@gmail.com>
2024-12-08 18:16:42 +0900

    가챠 페이지 : 모달 닫힐때 카드 배열 초기화 및 gacha layout 수정

--- src/app/gachas/[id]/gacha/components/GachaContainer.tsx
+++ src/app/gachas/[id]/gacha/components/GachaContainer.tsx
@@ -1,10 +1,9 @@
 'use client'
+import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import { useState } from 'react'
 
-import { cn } from '@/lib/utils'
-
-import { GachaResult } from './GachaResult'
+import { GachaResultModal } from './GachaResultModal'
 
 export const GachaContainer = () => {
   const [result, setResult] = useState<string | null>(null)
@@ -18,26 +17,31 @@ export const GachaContainer = () => {
     setIsLoading(true)
     setResult(null)
 
-    // 선택된 카드만 뒤집기
-    setFlippedIndex(index)
+    setFlippedIndex(null)
 
     setTimeout(() => {
-      // 선택된 카드에 대한 결과를 표시
-      const selectedCard = cards[index]
-      setResult(selectedCard)
+      // 스피너가 끝난 후 선택된 카드만 뒤집기
+      setFlippedIndex(index)
+      setResult(cards[index])
       setIsLoading(false)
     }, 2000)
 
     setTimeout(() => {
       setIsOpenModal(true)
-    }, 3000)
+    }, 2500)
+  }
+
+  const resetGacha = () => {
+    setFlippedIndex(null)
+    setResult(null)
+    setIsOpenModal(false)
+    setIsLoading(false)
   }
 
   return (
     <>
       {/* 카드 목록 */}
-      <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-yellow-200 to-blue-200">
-        {/* 상단 텍스트 */}
+      <div className="flex min-h-screen flex-col items-center justify-center">
         <p className="mb-[72px] text-2xl font-bold text-[#1F2024]">
           한 장의 카드를
           {' '}
@@ -53,17 +57,11 @@ export const GachaContainer = () => {
               <div
                 className={cn(
                   'relative h-44 w-32 transition-transform duration-700 transform-style-preserve-3d',
+                  isLoading && 'animate-spin',
                   flippedIndex === index && 'rotate-y-180',
                   isLoading && 'pointer-events-none opacity-50'
                 )}
               >
-                {/* 로딩 스피너 */}
-                {isLoading && (
-                  <div className="absolute inset-0 flex items-center justify-center">
-                    <div className="size-8 animate-spin rounded-full border-4 border-white border-t-transparent"></div>
-                  </div>
-                )}
-
                 {/* 카드 뒷면 */}
                 <div className="absolute flex cursor-pointer items-center justify-center shadow-md backface-hidden">
                   <Image src="/Card back.png" alt="card image" width={158} height={195} />
@@ -79,11 +77,9 @@ export const GachaContainer = () => {
         </div>
 
         {/* 결과 표시 */}
-        <GachaResult result={result} isOpenModal={isOpenModal} setIsOpenModal={setIsOpenModal} />
+        <GachaResultModal result={result} isOpenModal={isOpenModal} setIsOpenModal={setIsOpenModal} resetGacha={resetGacha} />
 
       </div>
-      {/* 다른 사람의 결과 */}
-      {/* <GachaAwrad /> */}
     </>
   )
 }
--- src/app/gachas/[id]/gacha/components/GachaResultModal.tsx
+++ src/app/gachas/[id]/gacha/components/GachaResultModal.tsx
@@ -1,9 +1,4 @@
 'use client'
-import Image from 'next/image'
-import { useRouter } from 'next/navigation'
-// import Confetti from 'react-confetti'
-// import { useWindowSize } from 'usehooks-ts'
-
 import { Button } from '@/components/ui/button'
 import {
   Dialog,
@@ -12,14 +7,21 @@ import {
   DialogFooter,
   DialogTitle,
 } from '@/components/ui/dialog'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+// import Confetti from 'react-confetti'
+// import { useWindowSize } from 'usehooks-ts'
+
+import { Dispatch, SetStateAction } from 'react'
 
 interface Props {
   result: string | null
   isOpenModal: boolean
-  setIsOpenModal: (isOpen: boolean) => void
+  setIsOpenModal: Dispatch<SetStateAction<boolean>>
+  resetGacha: () => void
 }
 
-export const GachaResult = ({ result, isOpenModal, setIsOpenModal }: Props) => {
+export const GachaResultModal = ({ result, isOpenModal, setIsOpenModal, resetGacha }: Props) => {
   // const { width, height } = useWindowSize()
 
   const navigate = useRouter()
@@ -29,7 +31,12 @@ export const GachaResult = ({ result, isOpenModal, setIsOpenModal }: Props) => {
       <Dialog open={isOpenModal} onOpenChange={setIsOpenModal}>
         {result && (
           <>
-            <DialogContent className="flex flex-col items-center gap-8 rounded-lg p-6 shadow-lg sm:max-w-[335px]">
+            <DialogContent
+              onClick={() => {
+                resetGacha()
+              }}
+              className="flex flex-col items-center gap-8 rounded-lg p-6 shadow-lg sm:max-w-[335px]"
+            >
               {/* <Confetti
                 width={width}
                 height={height}
@@ -47,10 +54,25 @@ export const GachaResult = ({ result, isOpenModal, setIsOpenModal }: Props) => {
                 <Image src="/Card 1.png" alt="card image" width={158} height={195} className="rounded-lg shadow-lg" />
               </div>
               <DialogFooter className="flex w-full gap-4">
-                <Button type="button" onClick={() => location.reload()} className="w-full rounded-lg text-white">
+                <Button
+                  type="button"
+                  onClick={() => {
+                    resetGacha()
+                  }}
+                  className="w-full rounded-lg text-white"
+                >
                   다시뽑기
                 </Button>
-                <Button type="button" variant="secondary" onClick={() => navigate.push('http://localhost:3000/gachas/1')} className="w-full border-2">
+                <Button
+                  type="button"
+                  variant="secondary"
+                  onClick={() => {
+                    // 배포되면 url 수정하기
+                    navigate.push('http://localhost:3000/gachas/1')
+                    resetGacha()
+                  }}
+                  className="w-full border-2"
+                >
                   되돌아가기
                 </Button>
               </DialogFooter>
