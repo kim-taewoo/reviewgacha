cd282d4d673c3c66b3c7fc5d0224cbcc43367a92
jes <suni980215@gmail.com>
2024-12-08 22:43:39 +0900

    랭킹 페이지 : tab 컴포넌트 생성 및 경로 수정

--- src/app/ranking/[id]/page.tsx
+++ src/app/ranking/[id]/page.tsx
@@ -4,19 +4,20 @@ import {
   TabsList,
   TabsTrigger,
 } from '@/components/ui/tabs'
+import { MyRanking } from '@/features/ranking/components/MyRanking'
 
 const RankingPage = () => {
   return (
     <Tabs defaultValue="myGacha" className="w-full">
       <TabsList className="grid grid-cols-2">
         <TabsTrigger value="myGacha">내 가챠</TabsTrigger>
-        <TabsTrigger value="myLanking">랭킹</TabsTrigger>
+        <TabsTrigger value="myRanking">랭킹</TabsTrigger>
       </TabsList>
       <TabsContent value="myGacha">
         myGacha
       </TabsContent>
-      <TabsContent value="myLanking">
-        myLanking
+      <TabsContent value="myRanking">
+        <MyRanking />
       </TabsContent>
     </Tabs>
   )
