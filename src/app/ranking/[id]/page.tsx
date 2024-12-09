e76a50d5393c47e752fa96992dedfc5bd67230e0
jes <suni980215@gmail.com>
2024-12-09 14:01:38 +0900

    랭킹 페이지 : 헤더 추가

--- src/app/ranking/[id]/page.tsx
+++ src/app/ranking/[id]/page.tsx
@@ -1,3 +1,7 @@
+import { getSupabaseServerClient } from '@/lib/supabase/server'
+import { Header } from '@/components/Header'
+import { Footer } from '@/components/Footer'
+
 import {
   Tabs,
   TabsContent,
@@ -6,20 +10,48 @@ import {
 } from '@/components/ui/tabs'
 import { MyRanking } from '@/features/ranking/components/MyRanking'
 
-const RankingPage = () => {
+type Params = Promise<{ id: string }>
+
+const RankingPage = async ({ params }: { params: Params }) => {
+  const { id } = await params
+
+  const supabase = await getSupabaseServerClient()
+  const { data: sessionData } = await supabase.auth.getSession()
+  const session = sessionData?.session
+  const user = session?.user
+
+  const { data: notUsedGachas, error } = await supabase
+    .from('gachas')
+    .select()
+    .match({
+      post_id: id,
+      is_used: false,
+      user_id: user?.id,
+    })
+
+  if (error) {
+    console.error(error)
+    return <div>에러가 발생했습니다</div>
+  }
+
   return (
-    <Tabs defaultValue="myGacha" className="w-full">
-      <TabsList className="grid grid-cols-2">
-        <TabsTrigger value="myGacha">내 가챠</TabsTrigger>
-        <TabsTrigger value="myRanking">랭킹</TabsTrigger>
-      </TabsList>
-      <TabsContent value="myGacha">
-        myGacha
-      </TabsContent>
-      <TabsContent value="myRanking">
-        <MyRanking />
-      </TabsContent>
-    </Tabs>
+    <>
+      <Header ticketCount={notUsedGachas.length} postId={id} />
+      <div className="flex w-full h-full flex-1 flex-col items-center gap-20 p-5 max-w-4xl">
+        <Tabs defaultValue="myGacha" className="w-full">
+          <TabsList className="grid grid-cols-2">
+            <TabsTrigger value="myGacha">내 가챠</TabsTrigger>
+            <TabsTrigger value="myRanking">랭킹</TabsTrigger>
+          </TabsList>
+          <TabsContent value="myGacha">myGacha</TabsContent>
+          <TabsContent value="myRanking">
+            <MyRanking />
+          </TabsContent>
+        </Tabs>
+      </div>
+      <Footer />
+    </>
   )
 }
+
 export default RankingPage
