d1324ebac1fe0dc52eebd6133397072e061c0889
kim-taewoo <acoustic0419@gmail.com>
2024-12-09 20:06:16 +0900

    style: bottom 네비게이션에 가려지는 부분 수정

--- src/app/ranking/[id]/page.tsx
+++ src/app/ranking/[id]/page.tsx
@@ -37,7 +37,7 @@ const RankingPage = async ({ params }: { params: Params }) => {
   return (
     <>
       <Header ticketCount={notUsedGachas.length} postId={id} />
-      <div className="flex size-full max-w-4xl flex-1 flex-col items-center gap-20 p-5">
+      <div className="flex size-full max-w-4xl flex-1 flex-col items-center gap-20 p-5 pb-20">
         <Tabs defaultValue="myGacha" className="w-full">
           <TabsList className="grid grid-cols-2">
             <TabsTrigger value="myGacha">내 가챠</TabsTrigger>
