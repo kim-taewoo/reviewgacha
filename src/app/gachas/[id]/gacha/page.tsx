fc19ce605b175560bd0b468aafc23203cdcf69ef
jes <suni980215@gmail.com>
2024-12-09 11:08:45 +0900

    build error 수정

--- src/app/gachas/[id]/gacha/page.tsx
+++ src/app/gachas/[id]/gacha/page.tsx
@@ -1,31 +1,22 @@
 import { GachaContainer } from '@/features/gacha/components/GachaContainer'
 import { getSupabaseServerClient } from '@/lib/supabase/server'
 
-type Params = Promise<{ id: string }>
-
-const GachaPage = async ({ params }: { params: Params }) => {
-  const { id } = await params
-
+const GachaPage = async ({ params }: { params: Promise<{ id: string }> }) => {
   const supabase = await getSupabaseServerClient()
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { data: sessionData, error: _sessionError } = await supabase.auth.getSession()
-  const session = sessionData?.session
-  const user = session?.user
-  const { data: notUsedGachas, error } = await supabase.from('gachas').select().match({
-    post_id: id,
+  const { data: unusedGachas, error } = await supabase.from('gachas').select().match({
     is_used: false,
-    user_id: user?.id,
   })
+
+  const { id: postId } = await params
 
   if (error) {
     console.error(error)
     return <div>에러가 발생했습니다</div>
   }
-  if (!user) return
 
   return (
     <div className="h-screen w-full bg-gradient-to-b from-yellow-200 to-blue-200">
-      <GachaContainer postId={id} userId={user.id} />
+      <GachaContainer unusedGachas={unusedGachas} postId={postId} />
     </div>
   )
 }
