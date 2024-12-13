4a6f210d8244cec6cd5a01c4c9fd0ffa548476d2
kim-taewoo <acoustic0419@gmail.com>
2024-12-14 01:17:14 +0900

    feat: loading 페이지 추가

---
+++ src/app/loading.tsx
@@ -0,0 +1,12 @@
+import { LoadingCircle } from '@/components/LoadingCircle'
+
+export default function Loading() {
+  return (
+    <div className="flex min-h-screen w-full flex-col items-center justify-center gap-3 text-[#FF9E49]">
+      <LoadingCircle className="text-[#FF9E49]" />
+      <div>
+        로딩중...
+      </div>
+    </div>
+  )
+}
